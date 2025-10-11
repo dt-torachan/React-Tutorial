9d035a09d66bce57813c423c786992bcc0fcfb78
y.katagiri <y.katagiri.artisan@gmail.com>
2025-10-11 10:07:53 +0900

    【Change】カウントアップでlocalStrageを利用するように

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  // const [count, setCount] = useState(0); 
+  const [count, setCount] = useState(() => {
+    // 初期値をlocalStorageから取得 なければ初期値0
+    const saved = localStorage.getItem("count");
+    return saved ? Number(saved) : 0;
+  });
+
+  useEffect(() => {
+    // countが変わるたびにlocalStorageへ保存
+    localStorage.setItem("count", count);
+  }, [count]);
+  
+  const handleClick = () => {
+    setCount(count + 1); // カウントアップ
+  };
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>カウントアップアプリ</h1>
+      <p>現在のカウント: {count}</p>
+      <button onClick={handleClick}>+1</button>
+    </div>
+  );
+}
+
+export default App;
