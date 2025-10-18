e564dd5a977a29a960319bc1190d081af3d49e17
y.katagiri <y.katagiri.artisan@gmail.com>
2025-10-18 10:27:48 +0900

    【Modify】カウントに任意の初期値をセットできるように

--- src/App.js
+++ src/App.js
@@ -10,6 +10,14 @@ function App() {
     return saved ? Number(saved) : 0;
   });
 
+  const [text, setText] = useState("");
+
+  const setInit = () => {
+    setCount(Number(text));
+    setText("");
+  };
+  
+
   useEffect(() => {
     // countが変わるたびにlocalStorageへ保存
     localStorage.setItem("count", count);
@@ -25,6 +33,13 @@ function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '100px' }}>
       <h1>カウントアップアプリ</h1>
+      <input
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        onKeyDown={(e) => e.key === "Enter" && setInit()}
+        placeholder="初期値を入力"
+      />
+      <button onClick={setInit}>初期値セット</button>
       <p>現在のカウント: {count}</p>
       <button onClick={handleClickCountUp}>+1</button>
       <button onClick={handleClickCountDown}>-1</button>
